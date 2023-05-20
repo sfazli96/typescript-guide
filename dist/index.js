f0f32f176b4174131e06c1b973ecf0c3f60347d1
sfazli96 <sfazli96@gmail.com>
2023-05-20 12:44:35 -0700

    Typescript basic types, arrays, objects, tuple, etc

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,33 @@
 "use strict";
+// Basic Types
 let id = 5;
-console.log("ID", id);
+let company = 'Sam';
+let isPublished = true;
+let x = 'Hello';
+x = true;
+let age;
+age = 30;
+// Arrays
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, 'Hello'];
+// Tuple
+let person = [1, 'Sam', true];
+// Tuple Array
+let employee;
+employee = [
+    [1, 'Sam'],
+    [2, 'John'],
+    [3, 'Jill']
+];
+// Union
+let pid = 22;
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["Up"] = 0] = "Up";
+    Direction1[Direction1["Down"] = 1] = "Down";
+    Direction1[Direction1["Left"] = 2] = "Left";
+    Direction1[Direction1["Right"] = 3] = "Right";
+})(Direction1 || (Direction1 = {}));
+console.log(Direction1.Up);
+// console.log("ID", id)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,63 @@
+// Basic Types
 let id: number = 5
+let company: string = 'Sam'
+let isPublished: boolean = true
+let x: any = 'Hello'
+x = true
+let age: number
+age = 30
 
-console.log("ID", id)
+// Arrays
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Sam', true]
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Sam'],
+    [2, 'John'],
+    [3, 'Jill']
+]
+
+// Union
+let pid: string | number = 22
+
+// Enum
+// enum Direction1 {
+//     Up,
+//     Down,
+//     Left,
+//     Right
+// }
+
+enum Direction1 {
+    Up = 'Up',
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+const user: User = {
+    id: 1,
+    name: 'John'
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Functions
+
+
+
+// console.log(Direction1.Up)
+// console.log("ID", id)
