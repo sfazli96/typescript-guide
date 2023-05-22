22da73547bfacc5f832317a521f7c1aeefaa1f42
sfazli96 <sfazli96@gmail.com>
2023-05-22 09:11:51 -0700

    typescript generics

--- src/index.ts
+++ src/index.ts
@@ -107,7 +107,15 @@ class Employee extends Person {
 
 const emp = new Employee(3, 'Shawn',  'Developer')
 
+// Generics
+function getArray<T>(items: T[]): T[] {
+    return new Array().concat(items)
+}
 
+let numArray = getArray<number>([1,2,3,4])
+let strArray = getArray<string>(['brad', 'John', 'Jill'])
+
+// numArray.push('hello')
 
 // console.log(brad.register())
 // console.log(emp.name)
